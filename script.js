4a7872384631b775cff0e387ce7a9d0fd0f948fe
mperkins1995 <mperkins1995@gmail.com>
2023-04-28 10:06:16 -0500

    rock paper scissors game

---
+++ script.js
@@ -0,0 +1,59 @@
+function getUserChoice() {
+    var userInput = prompt('Rock, Paper, or Scissors?');
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput;
+    } else {
+        console.log('Error!');
+    }
+}
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        default:
+            return 'scissors';
+    }
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return 'The game is a tie!';
+    }
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+}
+
+function playGame() {
+    const userChoice = getUserChoice('rock');
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw: ' + computerChoice);
+
+    console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
